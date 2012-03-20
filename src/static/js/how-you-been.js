a0119adc4888184bfb8f9ef6a5361056cee30faf
Tim Gilbert <timgilbert@gmail.com>
2012-03-19 23:26:36 -0400

    Function rearrangement for more readable file flow; pulling weight function into Track prototype

--- src/static/js/how-you-been.js
+++ src/static/js/how-you-been.js
@@ -36,6 +36,60 @@ function generatePlaylist() {
 }
 
 /**
+ * After a successful Ajax call, go through the resulting list and populate 
+ * the initial playlist.
+ */
+function parseInspiration(inspirationList) {
+  for (var i = 0; i < inspirationList.length; i++) {
+    var inspirationStore = new InspirationStore(inspirationList[i]);
+    $('#playlist').append(createTrack(inspirationStore, i));
+  }
+}
+
+/**
+ * Clone a single copy of the track div, attach data to it, and return it
+ */
+function createTrack(inspirationStore, index) {
+  var entry$ = $('#templates #track').clone();
+  
+  //entry$.data({'inspiration': inspirationStore, 'index': index});
+  $('.inspiration', entry$).text(inspirationStore.activeInspiration.value);
+  
+  // When we get a call back from last.fm, this handler will be called
+  entry$.bind('lastFmReady', function(event, trackStore) {
+    console.debug('lastFmReady', trackStore);
+    $('.inspiration', this).text(inspirationStore.activeInspiration.value);
+    if (trackStore.hits <= 0) {
+      // No hits found
+      $('.notfound', this).show();
+      return;
+    }
+    $('.found', this).show();
+    $('.tracksFound', this).text(trackStore.hits);
+    
+    var track = trackStore.best();
+    //console.debug(track);
+    $('.artist', this).text(track.artist);
+    $('.name', this).text(track.name);
+    $('.name', this).attr('href', track.url);
+    if (track.image) {
+      $('.thumbnail', this).attr('src', track.image);
+    }
+    $('button.search', this).removeAttr('disabled');
+  });
+  
+  // Button handler.  If this is successful, a 'lastFmReady' event will be fired 
+  // (see handler above)
+  $('button.search', entry$).click(function(event) {
+    $('button.search', this).attr('disabled', true);
+    inspirationStore.searchForTrack(entry$);
+  });
+  
+  //console.log(index + ": " + inspiration)
+  return entry$;
+}
+
+/**
  * Main interface to the inspiration list returned by the server.  Each track 
  * will have one of these attached.  Most tracks will only use the first one, 
  * but in case there are no hits on the first inspiration, we'll drop back to 
@@ -118,16 +172,8 @@ function TrackStore(data) {
   
   for (var i = 0; i < matches.length; i++) {
     var track = new Track(matches[i]);
-    var weight = 10;
+    var weight = track.weight();
     
-    // Prefer streamable tracks
-    if (track.streamable) {
-      weight *= 10;
-    }
-    // Prefer full tracks
-    if (track.fulltrack) {
-      weight *= 5;
-    }
     // TBD: prefer artists this user likes (via last.fm/tasteometer)
     // TBD (maybe) prefer globally popular tracks? via track.listeners
     this.trackList.push(track.key, weight, track);
@@ -156,9 +202,12 @@ function Track(matchData) {
   this.streamable = matchData.streamable['#text'] == '1';
   this.fullTrack = this.streamable && matchData.streamable['fulltrack'] == '1';
   
-  for (var i = 0; i < matchData.image; i++) {
-    if (matchData.image[i].size == 'large') {
-      this.image = matchData.image[i]['#text'];
+  if (typeof(matchData.image) !== 'undefined') {
+    for (var i = 0; i < matchData.image.length; i++) {
+      if (matchData.image[i].size == 'large') {
+        this.image = matchData.image[i]['#text'];
+        //console.debug('image', this.image, matchData.image[i]);
+      }
     }
   }
   
@@ -166,6 +215,27 @@ function Track(matchData) {
   this.key = this.artist + ' ** ' + this.name;
 }
 
+Track.prototype = {
+  /** Return a basic weight for this track */
+  weight: function() {
+    var result = 10;
+    
+    // Prefer streamable tracks
+    if (this.streamable) {
+      result *= 10;
+    }
+    // Prefer full tracks
+    if (this.fulltrack) {
+      result *= 5;
+    }
+    // Prefer tracks with images
+    if (this.image) {
+      result *= 10; 
+    }
+    
+    return result;
+  }
+}
 
 // Ah, javascript, you never cease to amaze me with your elegance
 // This is from http://stackoverflow.com/a/1404100/87990
@@ -178,53 +248,4 @@ function getURLParameter(name) {
 /** First pass at searching: synchronous */
 
 
-/**
- * Clone a single copy of the track div, attach data to it, and return it
- */
-function createTrack(inspirationStore, index) {
-  var entry$ = $('#templates #track').clone();
-  
-  //entry$.data({'inspiration': inspirationStore, 'index': index});
-  $('.inspiration', entry$).text(inspirationStore.activeInspiration.value);
-  
-  // When we get a call back from last.fm, this handler will be called
-  entry$.bind('lastFmReady', function(event, trackStore) {
-    console.debug('lastFmReady', trackStore);
-    $('.inspiration', this).text(inspirationStore.activeInspiration.value);
-    if (trackStore.hits <= 0) {
-      // No hits found
-      $('.notfound', this).show();
-      return;
-    }
-    $('.found', this).show();
-    $('.tracksFound', this).text(trackStore.hits);
-    
-    var track = trackStore.best();
-    console.debug(track);
-    $('.artist', this).text(track.artist);
-    $('.name', this).text(track.name);
-    $('.name', this).attr({href: track.url});
-    $('.image', this).attr({href: track.image});
-  });
-  
-  // Button handler.  If this is successful, a 'lastFmReady' event will be fired 
-  // (see handler above)
-  $('button.search', entry$).click(function(event) {
-    inspirationStore.searchForTrack(entry$);
-  });
 
-  //console.log(index + ": " + inspiration)
-  return entry$;
-}
-
-
-/**
- * After a successful Ajax call, go through the resulting list and populate 
- * the initial playlist.
- */
-function parseInspiration(inspirationList) {
-  for (var i = 0; i < inspirationList.length; i++) {
-    var inspiration = new InspirationStore(inspirationList[i]);
-    $('#playlist').append(createTrack(inspiration, i));
-  }
-}
